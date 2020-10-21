0fa27875f58f383729bc417f4a76a0cbd9f07ba8
shmutalov <shmutalov@gmail.com>
2020-10-21 18:06:14 +0500

    Added base lib header funcs

--- libccminer.h
+++ libccminer.h
@@ -1,4 +1,17 @@
 #ifndef _LIBCCMINER_H_
 #define _LIBCCMINER_H_
 
+#ifdef __cplusplus
+extern "C"{
+#endif //__cplusplus
+
+int init(void(*cb)(const char *format, va_list arg));
+int is_running();
+int start(const char* url, const char* user, const char* pass, const int n_threads=0);
+int stop();
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
 #endif
